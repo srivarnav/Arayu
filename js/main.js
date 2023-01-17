b2d3c489290d6629b8f4d6ffcccf2e0f515188cd
imvinay84 <vsvinaysingh82@gmail.com>
2023-01-18 02:46:35 +0530

    minute changes

--- js/main.js
+++ js/main.js
@@ -135,14 +135,10 @@
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     // Output the result in an element with id="demo"
-    document.getElementById("days").innerHTML =
-      days;
-      document.getElementById("hours").innerHTML =
-      hours;
-      document.getElementById("minutes").innerHTML =
-      minutes;
-      document.getElementById("seconds").innerHTML =
-      seconds;
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
 
     // If the count down is over, write some text
     if (distance < 0) {
